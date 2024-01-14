788df7efdb0a8ad0212a90634be06baf6885e585
Gil <gil@lavanet.xyz>
2024-01-14 10:31:53 -0500

    leap4

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended", "prettier"],
   rules: {
     curly: ["warn", "multi-line", "consistent"],
+    "no-explicit-any": "off",
     "no-bitwise": "warn",
     "no-console": "off",
     "no-param-reassign": "warn",
